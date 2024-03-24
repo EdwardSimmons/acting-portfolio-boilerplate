1227d125963c7dacbaeae8a37897b18cedb2231c
Ed Simmons <e.d.simmons@outlook.com>
2024-03-24 12:18:27 +0000

    Fix: include 404 in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { join, parse, resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/acting-portfolio-boilerplate/",
+  build: {
+    rollupOptions: {
+      input: entryPoints("index.html", "404.html"),
+    },
+  },
 })
+
+function entryPoints(...paths) {
+  const entries = paths.map(parse).map(entry => {
+    const { dir, base, name, ext } = entry
+    const key = join(dir, name)
+    const path = resolve(__dirname, dir, base)
+    return [key, path]
+  })
+
+  const config = Object.fromEntries(entries)
+  return config
+}
